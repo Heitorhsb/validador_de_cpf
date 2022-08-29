138e2a074eafc71df9de5f890d8d1e5dee6d5371
Heitor <heitor90henrique@gmail.com>
2022-08-29 13:39:19 -0300

    ADD VIA UPLOAD
    Código fonte do validador de CPF

---
+++ validador_de_CPF.py
@@ -0,0 +1,24 @@
+cpf = "29692002850"
+novo_cpf = cpf[:-2]
+
+reverso = 10
+total = 0
+for i in range(19):
+	if i > 8:
+		i -= 9
+	
+	total += int(novo_cpf[i]) * reverso
+
+	reverso -=1
+	if reverso <2:
+		reverso = 11
+		d = 11 - (total % 11)
+		if d > 9:
+			d = 0
+		total = 0
+		novo_cpf += str(d)
+
+if novo_cpf != cpf:
+	print("cpf invalido")
+else:
+	print("cpf valido")
